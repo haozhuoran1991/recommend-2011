262af4932e626ebfe863ddb56907589543909f51
neta.alima <neta.alima@gmail.com>
2012-05-04 06:09:19 +0000

    finished q 3.5

--- NLP1/q3_5.py
+++ NLP1/q3_5.py
@@ -1,6 +1,7 @@
 import  nltk
 import pylab
 from nltk.corpus import brown
+import random
 
 #plot distribution of sentences by length
 def getDistSentByLength():
@@ -19,13 +20,21 @@ def getDistSentByLength():
     pylab.grid(True)
     pylab.show()
 
+def getAllTaggedCorpus():
+    taggedSens = []
+    cats = brown.categories()
+    for cat in cats:
+        for sen in brown.tagged_sents(categories=cat):
+            taggedSens.append(sen)
+    return taggedSens
+        
 def divideToLengthClasses(cat='All'):
     shortC = []
     mediumC = []
     longC = []
     taggedSentences = []
     if cat == 'All':
-        taggedSentences = brown.tagged_sents()
+        taggedSentences = getAllTaggedCorpus()
     else:
         taggedSentences = brown.tagged_sents(categories=cat)
     for sen in taggedSentences:
@@ -39,33 +48,94 @@ def divideToLengthClasses(cat='All'):
     return [shortC, mediumC, longC]
     
 def stratifiedSamples(classes, N):
-    print "todo"
-
+    testSet = []
+    trainSet = []
+    for cl in classes:
+        noSenTest = int((float(N)/100)*len(cl))
+        while noSenTest > 0:
+            r = int(random.uniform(1,len(cl)-1))
+            testSet.append(cl.pop(r))
+            noSenTest -= 1
+        trainSet = trainSet + cl
+    return trainSet, testSet
+        
+def divideToGenereClasses():
+    classes = []
+    taggedSentences = []
+    cats = brown.categories()
+    for cat in cats:
+        for sen in brown.tagged_sents(categories=cat):
+            taggedSentences.append(sen)
+        classes.append(taggedSentences)
+    return classes
+    
 def main():
     #ploting the distribution graph
 #    getDistSentByLength()
+    #############################################################
+    #cycle of training-testing First case - Random split 90%-10%#
+    #############################################################
+    train, test = stratifiedSamples([getAllTaggedCorpus()], 10)
+
+    nn_tagger = nltk.DefaultTagger('NN')
+    regexp_tagger = nltk.RegexpTagger([(r'^-?[0-9]+(.[0-9]+)?$', 'CD'),   # cardinal numbers
+                                       (r'(The|the|A|a|An|an)$', 'AT'),   # articles
+                                       (r'.*able$', 'JJ'),                # adjectives
+                                       (r'.*ness$', 'NN'),                # nouns formed from adjectives
+                                       (r'.*ly$', 'RB'),                  # adverbs
+                                       (r'.*s$', 'NNS'),                  # plural nouns
+                                       (r'.*ing$', 'VBG'),                # gerunds
+                                       (r'.*ed$', 'VBD'),                 # past tense verbs
+                                       (r'.*', 'NN')                      # nouns (default)
+                                       ],backoff=nn_tagger)
+    at2 = nltk.AffixTagger(train, backoff=regexp_tagger)
+    ut3 = nltk.UnigramTagger(train, backoff=at2)
+    ct2 = nltk.NgramTagger(2, train, backoff=ut3)
+    print "evaluate bigram(unigram(affix(regExp(default nn)))) Random Split= " ,ct2.evaluate(test)
     
-    #dividing entire corpus to classes according to length
+    ###############################################################################################
+    #cycle of training-testing second case - Stratified split 90%-10% according to sentence length#
+    ###############################################################################################
     classes = divideToLengthClasses()
+    train, test = stratifiedSamples(classes, 10)
+
+    nn_tagger = nltk.DefaultTagger('NN')
+    regexp_tagger = nltk.RegexpTagger([(r'^-?[0-9]+(.[0-9]+)?$', 'CD'),   # cardinal numbers
+                                       (r'(The|the|A|a|An|an)$', 'AT'),   # articles
+                                       (r'.*able$', 'JJ'),                # adjectives
+                                       (r'.*ness$', 'NN'),                # nouns formed from adjectives
+                                       (r'.*ly$', 'RB'),                  # adverbs
+                                       (r'.*s$', 'NNS'),                  # plural nouns
+                                       (r'.*ing$', 'VBG'),                # gerunds
+                                       (r'.*ed$', 'VBD'),                 # past tense verbs
+                                       (r'.*', 'NN')                      # nouns (default)
+                                       ],backoff=nn_tagger)
+    at2 = nltk.AffixTagger(train, backoff=regexp_tagger)
+    ut3 = nltk.UnigramTagger(train, backoff=at2)
+    ct2 = nltk.NgramTagger(2, train, backoff=ut3)
+    print "evaluate bigram(unigram(affix(regExp(default nn)))) Length split = " ,ct2.evaluate(test)
     
-#    brown_news_tagged = brown.tagged_sents(categories='news')
-#    brown_train = brown_news_tagged[100:]
-#    brown_test = brown_news_tagged[:100]
-#    
-#    nn_tagger = nltk.DefaultTagger('NN')
-#    regexp_tagger = nltk.RegexpTagger([(r'^-?[0-9]+(.[0-9]+)?$', 'CD'),   # cardinal numbers
-#                                       (r'(The|the|A|a|An|an)$', 'AT'),   # articles
-#                                       (r'.*able$', 'JJ'),                # adjectives
-#                                       (r'.*ness$', 'NN'),                # nouns formed from adjectives
-#                                       (r'.*ly$', 'RB'),                  # adverbs
-#                                       (r'.*s$', 'NNS'),                  # plural nouns
-#                                       (r'.*ing$', 'VBG'),                # gerunds
-#                                       (r'.*ed$', 'VBD'),                 # past tense verbs
-#                                       (r'.*', 'NN')                      # nouns (default)
-#                                       ],backoff=nn_tagger)
-#    at2 = nltk.AffixTagger(brown_train, backoff=regexp_tagger)
-#    ut3 = nltk.UnigramTagger(brown_train, backoff=at2)
-#    ct2 = nltk.NgramTagger(2, brown_train, backoff=ut3)
+    #################################################################################################
+    #cycle of training-testing Third case - Stratified split 90%-10% according to the sentence genre#
+    #################################################################################################
+    classes = divideToGenereClasses()
+    train, test = stratifiedSamples(classes, 10)
+
+    nn_tagger = nltk.DefaultTagger('NN')
+    regexp_tagger = nltk.RegexpTagger([(r'^-?[0-9]+(.[0-9]+)?$', 'CD'),   # cardinal numbers
+                                       (r'(The|the|A|a|An|an)$', 'AT'),   # articles
+                                       (r'.*able$', 'JJ'),                # adjectives
+                                       (r'.*ness$', 'NN'),                # nouns formed from adjectives
+                                       (r'.*ly$', 'RB'),                  # adverbs
+                                       (r'.*s$', 'NNS'),                  # plural nouns
+                                       (r'.*ing$', 'VBG'),                # gerunds
+                                       (r'.*ed$', 'VBD'),                 # past tense verbs
+                                       (r'.*', 'NN')                      # nouns (default)
+                                       ],backoff=nn_tagger)
+    at2 = nltk.AffixTagger(train, backoff=regexp_tagger)
+    ut3 = nltk.UnigramTagger(train, backoff=at2)
+    ct2 = nltk.NgramTagger(2, train, backoff=ut3)
+    print "evaluate bigram(unigram(affix(regExp(default nn)))) Genere split = " ,ct2.evaluate(test)
     
 if __name__ == '__main__':
     main() 
