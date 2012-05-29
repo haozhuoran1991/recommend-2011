2c93cd6cd54b3ebf34ae9e34f81869f4bbd4229a
shimiml4@gmail.com <shimiml4@gmail.com@2ff91aee-6a6f-daa1-de4d-6b2726aa1a31>
2012-05-29 17:04:04 +0000

    finish

--- NLP2/q1_1.py
+++ NLP2/q1_1.py
@@ -1,14 +1,30 @@
-import  nltk
-from nltk.corpus import brown
 import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
+import math
 import numpy as np
 
-def xxx():
-    return
+def generateDataset(N, f, sigma):
+    x =np.linspace(0.0, 1.0, num=N) # (that is, x1 = 0, x2=1/N-1, x3=2/N-1..., xN = 1.0)
+    def ti(xi): return f(xi) + np.random.normal(0.0, sigma, 1) 
+    vti = np.vectorize(ti)
+    t = vti(x)
+    return (x,t)
 
+def makePlot(N, f, x, t):
+    vf = np.vectorize(f)
+    y = vf(x)
+    sizes = [40,50,70]
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    ax.scatter(x,t, s=sizes,marker = '.', facecolor='red' )
+    plt.plot(x,y)
+    plt.show()
+ 
+ 
 def main():
-    xxx()
+    N = 50
+    f = math.sin
+    (x,t) = generateDataset(N, f, 0.03);
+    makePlot(N, f, x, t)
     
 if __name__ == '__main__':
     main() 
