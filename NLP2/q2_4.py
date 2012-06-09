3a04eae2506e73133dd685d4607de57019932b57
neta.alima@gmail.com <neta.alima@gmail.com@2ff91aee-6a6f-daa1-de4d-6b2726aa1a31>
2012-06-09 14:48:52 +0000

    finished 2.4

--- NLP2/q2_4.py
+++ NLP2/q2_4.py
@@ -1,14 +1,93 @@
 import  nltk
+from nltk.evaluate import accuracy
 from nltk.corpus import brown
-import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
-import numpy as np
+from q2_1 import q2_1
+import pylab
 
-def xxx():
-    return
+#getting tagger trained by the brown corpus
+def getTrainedTagger():
+    train = brown.tagged_sents(simplify_tags=True)
+    newTrain = []
+    for sen in train:
+        newSen = []
+        for word,tag in sen:
+            newSen.append((word.lower(),tag))
+        newTrain.append(newSen)
+    nn_tagger = nltk.DefaultTagger('NN')
+    regexp_tagger = nltk.RegexpTagger([(r'^-?[0-9]+(.[0-9]+)?$', 'CD'),   # cardinal numbers
+                                       (r'(The|the|A|a|An|an)$', 'AT'),   # articles
+                                       (r'.*able$', 'JJ'),                # adjectives
+                                       (r'.*ness$', 'NN'),                # nouns formed from adjectives
+                                       (r'.*ly$', 'RB'),                  # adverbs
+                                       (r'.*s$', 'NNS'),                  # plural nouns
+                                       (r'.*ing$', 'VBG'),                # gerunds
+                                       (r'.*ed$', 'VBD'),                 # past tense verbs
+                                       (r'.*', 'NN')                      # nouns (default)
+                                       ],backoff=nn_tagger)
+    at2 = nltk.AffixTagger(newTrain, backoff=regexp_tagger)
+    ut3 = nltk.UnigramTagger(newTrain, backoff=at2)
+    ct2 = nltk.NgramTagger(2, newTrain, backoff=ut3)
+    return ct2
 
+def make_pos_extractor(pos):
+    tagger = getTrainedTagger()
+    def extractor(words):
+        taggedWords = tagger.tag(words)
+        return dict([(word, True) for word,tag in taggedWords if tag in pos])
+    return extractor
+#['ADJ', 'ADV', 'CNJ', 'DET', 'EX', 'FW', 'MOD', 'N', 'NP', 'NUM', 'PRO', 'P', 'TO', 'UH', 
+#'V', 'VD', 'VG', 'VN', 'WH']
 def main():
-    xxx()
+    q21 = q2_1()
+    x = []
+    y = []
+    pos = ['N', 'VG', 'ADJ', 'ADV']
+    print pos
+    extractor = make_pos_extractor(pos)
+    classifier = q21.evaluate_features(extractor, 10)
+    x.append(1)
+    acc = accuracy(q21.maintest, q21.testClassify)
+    y.append(acc)
+    
+    pos = ['N', 'V', 'VG', 'VN', 'VN', 'ADJ', 'ADV']
+    print pos
+    extractor = make_pos_extractor(pos)
+    classifier = q21.evaluate_features(extractor, 10)
+    x.append(2)
+    acc = accuracy(q21.maintest, q21.testClassify)
+    y.append(acc)
+    
+    pos = ['V', 'ADJ', 'ADV']
+    print pos
+    extractor = make_pos_extractor(pos)
+    classifier = q21.evaluate_features(extractor, 10)
+    x.append(3)
+    acc = accuracy(q21.maintest, q21.testClassify)
+    y.append(acc)
+    
+    pos = ['ADJ', 'ADV']
+    print pos
+    extractor = make_pos_extractor(pos)
+    classifier = q21.evaluate_features(extractor, 10)
+    x.append(4)
+    acc = accuracy(q21.maintest, q21.testClassify)
+    y.append(acc)
+    
+    pos = ['N', 'ADJ', 'ADV']
+    print pos
+    extractor = make_pos_extractor(pos)
+    classifier = q21.evaluate_features(extractor, 10)
+    x.append(5)
+    acc = accuracy(q21.maintest, q21.testClassify)
+    y.append(acc)
+    
+    pylab.bar(x, y, width=0.02, facecolor='blue', align='center')
+    pylab.xlabel('POS')
+    pylab.ylabel("Accuracy")
+    pylab.title("Accuracy for each pos set")
+    pylab.grid(False)
+    pylab.show()
+    return
     
 if __name__ == '__main__':
     main() 
