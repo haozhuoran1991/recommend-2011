fc7c30913e69ec000a0b202981c351ddc3664ee2
neta.alima@gmail.com <neta.alima@gmail.com@2ff91aee-6a6f-daa1-de4d-6b2726aa1a31>
2012-08-04 10:34:32 +0000

    removing stop words

--- wikification/src/program/App.java
+++ wikification/src/program/App.java
@@ -1,5 +1,6 @@
 package program;
 
+import extractWikiPages.Linguistic;
 import extractWikiPages.WikiCfd;
 import extractWikiPages.WikiData;
 
@@ -7,9 +8,11 @@ import extractWikiPages.WikiData;
 public class App {
 
 	 public static void main(String[] args){
-		 WikiData wikiData = new WikiData(200, 35);
-		 WikiCfd wikiCfd = new WikiCfd();
-		 wikiCfd.training(wikiData.getArticles());
+//		 WikiData wikiData = new WikiData(200, 35);
+//		 WikiCfd wikiCfd = new WikiCfd();
+//		 wikiCfd.training(wikiData.getArticles());
 		 
+		 String text = "אבא הלך לכן וסיפר לי סיפור עם אמא, הסיפור מספר על אבא של כלב שברח לכלב ונבח עליו.";
+		 Linguistic.segmentationAndStemming(text);
 	 }
 }
